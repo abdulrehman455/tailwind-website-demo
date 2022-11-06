ccd7f650374a57017c19361efe3bf322355650ff
abdulrehman455 <117396084+abdulrehman455@users.noreply.github.com>
2022-11-07 02:34:34 +0500

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./.*html'],
+  content: ['./*.html'],
   theme: {
    screens:{
       
-    sm:'480px',
+    sm:'400px',
     md:'750px',
     lg:'976px',
     xl:'1440px',
